9a8742dc5265d0d2430f5441b63066e876f69436
sunsjiun <149264526+sunsjiun@users.noreply.github.com>
2023-10-29 02:36:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/"C:\Users\Lenovo\Desktop\bb.jpg"",
+        "心爱的冰冰": "./imgs/"C:\Users\Lenovo\Desktop\bb.jpg"",
         // "今天是你的生日": "./imgs/"C:\Users\Lenovo\Desktop\bb.jpg"",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
